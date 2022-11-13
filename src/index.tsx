283a4f9b68472225fbc14efcd7f3a074b551c0a5
acongm <acongm@126.com>
2022-11-14 01:38:34 +0800

    feat: add ts

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { h, render } from 'preact';
 
 export const App = () => {
-  return <div>hello parcel + preact</div>;
+  return <div>hello parcel + preact + ts</div>;
 };
 const renderApp = () => render(<App />, document.getElementById('app'));
 export default renderApp;
