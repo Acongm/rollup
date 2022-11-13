97deb51d1a04912cbf58a7be8f76375a91946b0e
acongm <acongm@126.com>
2022-11-13 22:08:13 +0800

    feat: init

--- src/view/index.tsx
+++
@@ -1,8 +0,0 @@
-import { h, render } from 'preact';
-import View from './preact';
-import './style.css';
-
-const renderApp = (dom: Element, opt: any) => {
-  return render(<View />, dom);
-};
-export default renderApp;
